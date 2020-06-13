4944bbd2bc08ad81a0462cfcc28ecc875fd0ff34
ysawiris <youssef.sawiris@students.makeschool.com>
2020-06-12 17:42:15 -0700

    update the save function

--- makesite.go
+++ makesite.go
@@ -1,28 +1,84 @@
 package main
 
-<<<<<<< HEAD
 import (
+	"flag"
 	"io/ioutil"
+	"os"
+	"strings"
+	"text/template"
 )
 
-func main() {
-
+//
+type post struct {
+	Description string
 }
 
-func readFile() string {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+func readFile(filename string) string {
+	// Reading a File
+	fileContents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// A common use of `panic` is to abort if a function returns an error
+		// value that we don’t know how to (or want to) handle. This example
+		// panics if we get an unexpected error when creating a new file.
+		panic(err)
+	}
+	return string(fileContents)
+}
+
+func writeFile(fileContents string) {
+	//Writing a File
+	bytesToWrite := []byte(fileContents)
+	err := ioutil.WriteFile("new-file1.txt", bytesToWrite, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+func renderTemplate(filename string, data string) {
+	// var that holds content
+	c := post{Description: data}
+	// render given content in the template
+	t := template.Must(template.New("template.tmpl").ParseFiles(filename))
+
+	var err error
+	// Print out usind Stdout
+	err = t.Execute(os.Stdout, c)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func changeExtHTML(filename string) string {
+	// var to hold ext ending
+	ext := ".html"
+	// Deletes extension ending and adds new one
+	withExt := strings.Split(filename, ".")[0] + ext
+	return withExt
+}
+
+func writeTemplateToFile(tmplName string, data string) {
+	c := post{Description: readFile(data)}
+	t := template.Must(template.New("template.tmpl").ParseFiles(tmplName))
+
+	file := changeExtHTML(data)
+	f, err := os.Create(file)
 	if err != nil {
 		panic(err)
 	}
 
-	return string(fileContents)
+	err = t.Execute(f, c)
+	if err != nil {
+		panic(err)
+	}
 }
-
-func renderTemplate()
-=======
-import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	filePtr := flag.String("file", "", "txt file to be converted to html file")
+	flag.Parse()
+	if *filePtr != "" {
+		renderTemplate("template.tmpl", readFile(*filePtr))
+		writeTemplateToFile("template.tmpl", *filePtr)
+	} else {
+		renderTemplate("template.tmpl", readFile("first-post.txt"))
+		writeTemplateToFile("template.tmpl", "test.txt")
+	}
 }
->>>>>>> 9514ac8a2c135a448a2b15a4b246dcd5d59ee7bf
